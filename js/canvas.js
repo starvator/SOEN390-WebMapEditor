f26a23098c8d328d30979cb8228b85e758bba410
Tyler Ramsay <Tyler@Tylers-MacBook-Pro.local>
2016-01-30 14:39:37 -0500

    Updated the floor form, handled the submit, added the floor to canvas
    Updated the add new floor form
    Handled the submittal of the form to call the proper function
    removed event listeners
    Added the new floor plan to the canvas

--- js/canvas.js
+++ js/canvas.js
@@ -4,7 +4,7 @@ $(function(){
 
 	var img = new Image();
 	img.onload = function() {
-		ctx.drawImage(img, 0, 0);
+		ctx.drawImage(img, -1000, -1000);
 	}
 	img.src = "floor_plans/floor3.svg";
 });
--- js/floorList.js
+++ js/floorList.js
@@ -1,11 +1,19 @@
 $(function(){
-	//event listeners
-	document.getElementById('file').addEventListener('change', addSvgToList, false);
+    //handle the new floor submit
+    $('#newFloor').submit(function () {
+        
+        //if no file selected
+        if (!$("#fileUpload")[0].files[0]){
+            alert("you must select a file");
+            return false;
+        }
+        addSvgToList($("#fileUpload")[0].files[0]);
+        return false;
+    });
 });
 
 //Browse for a svg file and add it to the list of images, inspired by http://www.html5rocks.com/en/tutorials/file/dndfiles/
-function addSvgToList(evt){
-    var file = evt.target.files[0]; // The svg file
+function addSvgToList(file){
 
     // Only process svg files.
     if (!file.type.match('.svg')) {
@@ -17,10 +25,23 @@ function addSvgToList(evt){
     // Closure to capture the file information.
     reader.onload = (function() {
 		return function(e) {
-			$("#floorList").append('<li><img src="' +e.target.result+ '" width="100"/></li>');
+			addToCanvas(e.target.result);
+            $("#floorList").append('<li><img src="' +e.target.result+ '" width="100"/></li>');
 		};
     })(file);
 
     // Read in the image file as a data URL.
     reader.readAsDataURL(file);   
+}
+
+//take a svg string and add it to the canvas
+function addToCanvas(svgFile){
+	var can = document.getElementById('floorPlan');
+	var ctx = can.getContext('2d');
+
+	var img = new Image();
+	img.onload = function() {
+		ctx.drawImage(img, 0, 0);
+	}
+	img.src = svgFile;
 }
