52c5d43165acf780c6ebc58853e07a5161432ff7
Tyler Ramsay <Tyler@Tylers-MacBook-Pro.local>
2016-02-15 23:25:02 -0500

    Updating the ckeditor folder
    Removed the useless files

--- ckeditor/config.js
+++ ckeditor/config.js
@@ -4,7 +4,22 @@
  */
 
 CKEDITOR.editorConfig = function( config ) {
-	// Define changes to default configuration here. For example:
-	// config.language = 'fr';
-	// config.uiColor = '#AADC6E';
-};
+	config.toolbarGroups = [
+		//{ name: 'clipboard', groups: [ 'clipboard', 'undo' ] },
+		//{ name: 'editing', groups: [ 'find', 'selection', 'spellchecker', 'editing' ] },
+		//{ name: 'forms', groups: [ 'forms' ] },
+		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
+        { name: 'colors', groups: [ 'colors' ] },
+		//{ name: 'links', groups: [ 'links' ] },
+		{ name: 'insert', groups: [ 'insert' ] },
+		{ name: 'styles', groups: [ 'styles' ] },
+		//{ name: 'tools', groups: [ 'tools' ] },
+        { name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align', 'bidi', 'paragraph' ] },
+        { name: 'document', groups: [ 'mode', 'document', 'doctools' ] },
+		//{ name: 'others', groups: [ 'others' ] }
+	];
+
+	config.removeButtons = 'BidiRtl,BidiLtr,CreateDiv,HorizontalRule,Preview,Templates,Save,NewPage,Print,Form,Checkbox,Radio,TextField,Textarea,Select,Button,ImageButton,HiddenField,Flash,PageBreak,Iframe,About,Language,Anchor,Smiley';
+	config.language = 'en';
+	//config.language = 'fr-ca';
+	};
