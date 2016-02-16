0db990335e7f39b333a571dbfe454b6e8f413817
Tyler Ramsay <Tyler@Tylers-MacBook-Pro.local>
2016-02-15 21:48:47 -0500

    Added upload support
    You can now select a file using the attach a doc feature.

--- js/textEditor.js
+++ js/textEditor.js
@@ -1,9 +1,30 @@
 $(document).ready(function(){
+    //The open button
     $("#fakeStoryPoint").click(function(){
 		CKEDITOR.instances["editor1"].setData($('#fakeStoryPointText').val());
 		$("#infoEditingForm").show();
     });
+    
+    //The x
     $("#infoEditingFormClose").click(function(){
         $("#infoEditingForm").hide();
+        alert($("#uploadFile").val());
     });
+    
+    //When you click the attach button
+    $("#attachADoc").click(function(){
+        $("#uploadFile").trigger('click');
+    });
+    
+    //On change on input
+    $( "#uploadFile" ).change(function() {
+        try{
+        $("#attachedDocName").text($('#uploadFile')[0].files[0].name);
+        }
+        //if you click cancel in a dialog, it will clear the input, so clear the field
+        catch (e){
+            $("#attachedDocName").text("");
+        }
+    });
+    
 });
