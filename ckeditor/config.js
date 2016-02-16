2c89cd5ebee031c9b9f10bbe6fa5dd5bf9ce8f67
Tyler Ramsay <Tyler@Tylers-MacBook-Pro.local>
2016-02-15 22:06:13 -0500

    Changed the width, and customized the buttons
    Changed the width of the editor, and updated the toolbar buttons

--- ckeditor/config.js
+++ ckeditor/config.js
@@ -5,21 +5,21 @@
 
 CKEDITOR.editorConfig = function( config ) {
 	config.toolbarGroups = [
-		{ name: 'document', groups: [ 'mode', 'document', 'doctools' ] },
 		//{ name: 'clipboard', groups: [ 'clipboard', 'undo' ] },
-		{ name: 'editing', groups: [ 'find', 'selection', 'spellchecker', 'editing' ] },
+		//{ name: 'editing', groups: [ 'find', 'selection', 'spellchecker', 'editing' ] },
 		//{ name: 'forms', groups: [ 'forms' ] },
 		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
-		{ name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align', 'bidi', 'paragraph' ] },
-		{ name: 'links', groups: [ 'links' ] },
+        { name: 'colors', groups: [ 'colors' ] },
+		//{ name: 'links', groups: [ 'links' ] },
 		{ name: 'insert', groups: [ 'insert' ] },
 		{ name: 'styles', groups: [ 'styles' ] },
-		{ name: 'colors', groups: [ 'colors' ] },
-		{ name: 'tools', groups: [ 'tools' ] },
-		{ name: 'others', groups: [ 'others' ] }
+		//{ name: 'tools', groups: [ 'tools' ] },
+        { name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align', 'bidi', 'paragraph' ] },
+        { name: 'document', groups: [ 'mode', 'document', 'doctools' ] },
+		//{ name: 'others', groups: [ 'others' ] }
 	];
 
-	config.removeButtons = 'Save,NewPage,Print,Form,Checkbox,Radio,TextField,Textarea,Select,Button,ImageButton,HiddenField,Flash,PageBreak,Iframe,About,Language,Anchor,Smiley';
+	config.removeButtons = 'BidiRtl,BidiLtr,CreateDiv,HorizontalRule,Preview,Templates,Save,NewPage,Print,Form,Checkbox,Radio,TextField,Textarea,Select,Button,ImageButton,HiddenField,Flash,PageBreak,Iframe,About,Language,Anchor,Smiley';
 	config.language = 'en';
 	//config.language = 'fr-ca';
 	};
