d7793d59a997453a106f11082522d08c85a989cb
hoboman <pmiriello@gmail.com>
2016-02-19 17:03:26 -0500

    Fixed special character case.
    Could be dangerous to have unsupported characters, so I removed it.

--- js/compute.js
+++ js/compute.js
@@ -4,7 +4,7 @@ function distance(p1, p2) {
 	return Math.sqrt(Math.pow(p1.x - p2.x,2) + Math.pow(p1.y - p2.y,2));
 }
 
-//Credit to Matej Pokorný and mikemaccana for assistance
+//Credit to Matej Pokorny and mikemaccana for assistance
 function download(filename, text) {
   var element = document.createElement('a');
   element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
