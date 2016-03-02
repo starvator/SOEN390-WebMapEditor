920d56dbacf99aeedb00fef32a5ccbb6163ddad0
Disva <dravirdisva@gmail.com>
2016-03-02 13:27:38 -0500

    Added basic support for storyline descriptions

--- js/storylines.js
+++ js/storylines.js
@@ -5,20 +5,29 @@ var active_id=-2;
 function addNewStoryLine(){
 
     var name;
+	var description;
     name = $("#storylineField").val();
+	description = $("#storylineDescription").val();
     if(name != false){
         var storyline = new Storyline();
-
+		if(description != false){
+		$("#StorylinesList").append('<li id="'+ current_id +'" onclick="storylineClicked(this)"><a href="#">'+ name +'</a><a href="#">' + description +'</a></li>' +
+        '<ul id="'+ current_id +'_pointList"></ul>');
+		}
+		else{
         $("#StorylinesList").append('<li id="'+ current_id +'" onclick="storylineClicked(this)"><a href="#">'+ name +'</a></li>' +
         '<ul id="'+ current_id +'_pointList"></ul>');
+		}
         $("#StorylinesList").find(".active").removeClass("active");
         $("#"+current_id).addClass("active");
         active_id = current_id;
         storyline.ID = current_id;
         storyline.title.addPair('en', name);
+		storyline.description.addPair('en', description);
 
         current_id++;
         $("#storylineField").val("");
+		$("#storylineDescription").val("");
         storylineList.push(storyline);
     }
     else{
