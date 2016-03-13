c9ac577db26766f698c3c8e08a956a8d4dae1cf8
hoboman <pmiriello@gmail.com>
2016-03-13 19:59:41 -0400

    Revert "Added file Upload functionality"
    This reverts commit d225d515cc70be124d655672e0af94bbec658b54.

--- js/json.js
+++ js/json.js
@@ -1,4 +1,4 @@
-var jsonMap;
+
 
 function createJSON() {
     /*
@@ -17,33 +17,3 @@ function createJSON() {
         storyline:storylineList
         });
 }
-
-function loadFromJSON() {
-    floorList = jsonMap.floorPlan;
-    POIList = jsonMap.poi;
-    POTList = jsonMap.pot;
-    edgeList = jsonMap.edge;
-    storylineList = jsonMap.storyline;
-}
-
-$(document).on('change', '.btn-file :file', function() {
-    var input = $(this);
-    input.trigger('fileselect', [input.get(0).files[0]]);
-})
-
-$(document).ready( function() {
-    $('.btn-file :file').on('fileselect', function(event, file) {
-        if(!file) {
-            //TODO: custom file read alert
-            return;
-        }
-        var reader = new FileReader();
-        reader.onload = function(e) {
-            var contents = e.target.result;
-            jsonMap = JSON.parse(contents);
-            loadFromJSON();
-            redraw();
-        };
-        reader.readAsText(file);
-    });
-});
