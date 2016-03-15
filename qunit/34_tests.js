2d769330391bb727f0f370e6e9a6438010d8d27a
dsvoid <dsvoid@gmail.com>
2016-03-15 01:45:20 -0400

    Code coverage with blanket js fixed

---
+++ qunit/34_tests.js
@@ -0,0 +1,3 @@
+QUnit.test( "Sprint 3 user story 2.2: editing point details (stairs, bathrooms)", function( assert ) {
+	assert.equal( 1 == "1");
+});
---
+++ qunit/s4_tests.js
@@ -0,0 +1,20 @@
+//remove extraneous comments (like this one) before pushing!
+QUnit.test( "Sprint 4 user story 2.2: point editing (bathroom, stair, etc)", function( assert ) {
+    //create one new node of each type. Assert their existence in POTList.
+    assert.ok( 1 == "0" , "A new node of each type has been made.");
+    //edit the type of each node to another type. Assert that they are then changed in POTList.
+    assert.ok( 1 == "0", "Each node has been edited.");
+});
+
+QUnit.test( "Sprint 4 user story 4.4: showing and hiding storylines", function( assert ) {
+    //create two storylines. toggle their visible state.
+    //Assert that no storyline is visible in GUI.
+    //Assert that one storyline is visible in the GUI.
+    //Assert that both storylines are visible in the GUI.
+    assert.ok( 1 == "0");
+});
+
+QUnit.test( "Sprint 4 user story 5.2: loading a saved map from JSON", function( assert ) {
+    //read up on unit testing file I/O
+    assert.ok( 1 == "0");
+});
