01c0dabe2b62f460c30b15ea12bdc29f60889fef
dsvoid <dsvoid@gmail.com>
2016-03-28 18:07:32 -0400

    2.3 tests

--- qunit/s5_tests.js
+++ qunit/s5_tests.js
@@ -1,10 +1,31 @@
 QUnit.test( "Sprint 5 user story 2.3: point deletion", function( assert ) {
-	assert.ok( 1 == "0", "point successfully deleted");
+	//generate a new point on the map
+	nodeEditingMode = true;
+    mouseOnNode = false;
+    lastSelectedNode = false;
+    var delX = 600;
+    var delY = 600;
+	current_tool = "stair";
+	current_node_tool = "point";
+	canvasClick(delX,delY);
+	var numNodes = nodeList.length;
+	var numPots = POTList.length;
+	var lastNode = nodeList[nodeList.length - 1];
+	//remove the point and assert that it does not exist in any relevant list
+	deleteNode(nodeList[nodeList.length - 1]);
+	assert.ok( nodeList.length == numNodes - 1, "node object successfully deleted");
+	assert.ok( POTList.length == numPots - 1, "POT object successfully deleted");
+	
+	//connect one node to another with an edge
+	edgeList.push(new Edge(nodeList[0],nodeList[1]));
+	var numEdges = edgeList.length;
+	//delete one of the nodes and assert that the edge is also deleted
+	deleteNode(nodeList[1]);
+	assert.ok( edgeList.length == numEdges - 1, "edge connected to a deleted node has also been deleted");
 });
 
 QUnit.test( "Sprint 5 user story 2.4: point movement", function( assert ) {
 	assert.ok( 1 == "0", "point successfully moved");
-	assert.ok( 1 == "0", "paths to point remain in correct position");
 });
 
 QUnit.test( "Sprint 5 user story 3.2: independent path placement", function( assert ) {
