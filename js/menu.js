10a5c127190be51084c57c6dd92752be2e338fcf
hoboman <pmiriello@gmail.com>
2016-04-02 14:40:11 -0700

    Resolved Conflict
    paired review with @LambHoot

--- js/menu.js
+++ js/menu.js
@@ -183,6 +183,16 @@ function deleteActiveStoryline(){
             });
 }
 
+function deleteCurrentFloor(){
+	bootbox.confirm("Deleting a floor will delete all data associated with it, including points, POTs, POIs and StoryPoint information. Are you sure you want to delete this floor?", function(result) {
+	if(!result){
+		return;
+	}else{
+		deleteFloor();
+	}
+	return result;
+});
+
 function changeLanguage() {
     currentLanguage = $("#languageDropdown :selected").val();
 }
